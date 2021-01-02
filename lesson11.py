98c19f561b9cd8495f51bae5b28287c7eafa0393
hiroko-okada <yuru.yuru.okadasan@gmail.com>
2021-01-02 18:00:09 +0900

    strings

---
+++ lesson11.py
@@ -0,0 +1,13 @@
+
+print('hello')
+print("hello")
+print("I don't know")
+print('say "I don\'t know"')
+print(r'C:\name\name')
+print("""
+line1
+line2
+line3
+""")
+print('Hi.'*3)
+print('py'+'thon')
